ba45e964e84696ff4f5ca137cfa0275e93e30cd7
fampa <fampa@RebecaLT>
2023-09-10 20:36:09 -0600

    proyecto_03

--- src/Juego/Tablero.java
+++ src/Juego/Tablero.java
@@ -1,10 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package Juego;
 
-import Juego.Botons;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -15,36 +11,10 @@ import java.awt.event.MouseEvent;
  */
 public class Tablero extends javax.swing.JFrame {
 
-    private Botons[][] botons;
 
     public Tablero() {
         initComponents();
         this.setLocationRelativeTo(null);
-        this.botons= new Botons[10][10];
-        initBotones();
-    }
-
-    public void initBotones() {
-        tableroGUI.setLayout(new GridLayout(10, 10));
-        int botonsCount = 0;
-        for (int f = 0; f < 10; f++) {
-            for (int c = 0; c < 10; c++) {
-                botons[f][c] = new Botons(f, c, botonsCount, "_", "");
-                botons[f][c].setName("boton_" + String.valueOf(botonsCount));
-                botons[f][c].setBorder(null);
-                botonsCount++;
-                botons[f][c].addMouseListener(new MouseAdapter() {
-                    public void mouseClicked(MouseEvent evt) {
-                        botonesMouseClicked(evt);
-                    }
-                });
-                tableroGUI.add(botons[f][c]);
-            }
-        }
-    }
-
-    private void botonesMouseClicked(MouseEvent evt) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @SuppressWarnings("unchecked")
@@ -52,22 +22,20 @@ public class Tablero extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        tableroGUI = new javax.swing.JPanel();
+        tablero = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(102, 0, 0));
 
-        tableroGUI.setBackground(new java.awt.Color(0, 0, 102));
-
-        javax.swing.GroupLayout tableroGUILayout = new javax.swing.GroupLayout(tableroGUI);
-        tableroGUI.setLayout(tableroGUILayout);
-        tableroGUILayout.setHorizontalGroup(
-            tableroGUILayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout tableroLayout = new javax.swing.GroupLayout(tablero);
+        tablero.setLayout(tableroLayout);
+        tableroLayout.setHorizontalGroup(
+            tableroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 771, Short.MAX_VALUE)
         );
-        tableroGUILayout.setVerticalGroup(
-            tableroGUILayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        tableroLayout.setVerticalGroup(
+            tableroLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 748, Short.MAX_VALUE)
         );
 
@@ -77,14 +45,14 @@ public class Tablero extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(tableroGUI, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(tablero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(585, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(tableroGUI, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(tablero, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -159,6 +127,6 @@ public class Tablero extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel tableroGUI;
+    private javax.swing.JPanel tablero;
     // End of variables declaration//GEN-END:variables
 }
